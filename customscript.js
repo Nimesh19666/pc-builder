2f6137aa5766f2477625bca8238e99f650f1a145
Nimesh19666 <gujarinimesh-cmpn@atharvacoe.ac.in>
2023-04-06 00:01:35 +0530

    Uploading all files

---
+++ customscript.js
@@ -0,0 +1,224 @@
+
+const product = [
+    {
+        id: 0,
+        image: '/Users/nimesh/pc-builder/img/pcparts/AMD-RYZEN-7-3700X.png',
+        title: 'AMD RYZEN 5 3700X PROCESSOR',
+        price: 60000,
+    },
+    {
+        id: 1,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Amd-Ryzen-5-5600X.jpeg',
+        title: 'AMD RYZEN 5 5600X PROCESSOR',
+        price: 40000,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Amd-Ryzen-9-5900X.jpeg',
+        title: 'AMD RYZEN 9 5900X PROCESSOR',
+        price: 80000,
+    },
+    {
+        id: 3,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Intel-Core-i5-12600K-12th.jpeg',
+        title: 'INTEL CORE I5-12600K 12TH GEN',
+        price: 28000,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Intel-Core-I9-12900KF.jpeg',
+        title: 'INTEL CORE I9-12900KF',
+        price: 40000,
+    },
+    {
+        id: 0,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asrock-B550-PG-Velocita.jpeg',
+        title: 'ASROCK B550 PG VELOCITA ATX',
+        price: 27000,
+    },
+    {
+        id: 1,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asrock-B550-Steel-Legend.jpeg',
+        title: 'Asrock B550 Steel Legend Motherboard',
+        price: 35000,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asus-ROG-Strix-B550.jpeg',
+        title: 'ASUS ROG STRIX B550-F',
+        price: 24000,
+    },
+    {
+        id: 3,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asus-Prime-H670-PLUS.jpeg',
+        title: 'ASUS PRIME H670-PLUS',
+        price: 18000,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asus-Prime-B660M.jpeg',
+        title: 'ASUS PRIME B660M-A D4 INTEL',
+        price: 16000,
+    },
+    {
+        id: 0,
+        image: '/Users/nimesh/pc-builder/img/pcparts/G.SKILL-16GB-8GBX2-3200-MHZ-DDR4-TRIDENT-Z-NEO-RGB-RAM-2.jpg',
+        title: 'GSKILL RGB 16GB DDR5 3200MHz',
+        price: 7200,
+    },
+    {
+        id: 1,
+        image: '/Users/nimesh/pc-builder/img/pcparts/GSKILL-TRIDENT-Z5-RGB-32GB16GBx2-DDR5-5600MHz-CL36-RAM-BLACK-1.webp',
+        title: 'GSKILL RGB 32GB DDR5 5600MHz',
+        price: 8600,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/G.SKILL-16GB-8GBX2-3200-MHZ-DDR4-TRIDENT-Z-NEO-RGB-RAM-2.jpg',
+        title: 'GSKILL RGB 16GB DDR5 3200MHz',
+        price: 10000,
+    },
+    {
+        id: 3,
+        image: '/Users/nimesh/pc-builder/img/pcparts/G.SKILL-16GB-8GBX2-3200-MHZ-DDR4-TRIDENT-Z-NEO-RGB-RAM-2.jpg',
+        title: 'GSKILL RGB 16GB DDR5 3600MHz',
+        price: 8000,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Crucial-P2-250GB-M.2-NVMe-1.jpg',
+        title: 'CRUCIAL P2 250GB M.2 NVMe',
+        price: 4500,
+    },
+    {
+        id: 0,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Crucial-P2-1TB-M.2-NVMe-Internal-SSD-1.jpg',
+        title: 'CRUCIAL P2 1TB M.2 NVME INTERNAL SSD',
+        price: 3500,
+    },
+    {
+        id: 1,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Crucial-P1-1000GB-M.2-NVMe-1.jpg',
+        title: 'CRUCIAL P1 1 TB M.2 NVMe',
+        price: 2600,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Crucial-P1-3D-NAND-500GB-M.2-1.jpg',
+        title: 'CRUCIAL P1 3D NAND 500 GB M.2',
+        price: 3600,
+    },
+    {
+        id: 3,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Adata-8GB-8GBx1-DDR5-4800MHz-RAM-1.jpeg',
+        title: 'CRUCIAL P1 3D NAND 500 GB M.2',
+        price: 5600,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asus-Dual-GeForce-RTX-2060-Evo-6GB-GDDR6-1.png',
+        title: 'ASUS DUAL GEFORCE RTX 2060 EVO',
+        price: 60000,
+    },
+    {
+        id: 3,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asus-Prime-B660M.jpeg',
+        title: 'ASUS ROG STRIX RTX 3080 TI',
+        price: 80000,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Asus-Rog-Strix-RTX-3080ti-OC-12GB-1.png',
+        title: 'ASUS ROG STRIX RTX 3080 TI',
+        price: 70000,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/MSI-GeForce-RTX-3090-VENTUS-3X-24G-OC-1.png',
+        title: 'MSI GeForce RTX 3090 VENTUS',
+        price: 40000,
+    },
+    {
+        id: 3,
+        image: '/Users/nimesh/pc-builder/img/pcparts/INNO3D-GEFORCE-RTX-3070-ICHILL-X4-LHR-1.png',
+        title: 'INNO3D GEFORCE RTX 3070',
+        price: 4500,
+    },
+    {
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pcparts/Cooler-Master-MasterBox-K501L-RGB-Black-1.jpeg',
+        title: 'NZXT CABINET H210 (Matte Black)',
+        price: 6500,
+    },{
+        id: 2,
+        image: '/Users/nimesh/pc-builder/img/pc7.png',
+        title: 'NZXT H510 ELITE (Matte Black)',
+        price: 2300,
+    },
+    {
+        id: 3,
+        image: '/Users/nimesh/pc-builder/img/pc5.png',
+        title: 'NZXT H510 FLOW COMPACT',
+        price: 9999,
+    },
+   
+];
+
+const categories = [...new Set(product.map((item)=>
+    {return item}))]
+    let i=0;
+document.getElementById('root').innerHTML = categories.map((item)=>
+{
+    var {image, title, price} = item;
+    return(
+        `<div class='box'>
+            <div class='img-box'>
+                <img class='images' src=${image}></img>
+            </div>
+        <div class='bottom'>
+        <p>${title}</p>
+        <h2>₹ ${price}.00</h2>`+
+        "<button onclick='addtocart("+(i++)+")'>Add to cart</button>"+
+        `</div>
+        </div>`
+    )
+}).join('')
+
+var cart =[];
+
+function addtocart(a){
+    cart.push({...categories[a]});
+    displaycart();
+}
+function delElement(a){
+    cart.splice(a, 1);
+    displaycart();
+}
+
+function displaycart(){
+    let j = 0, total=0;
+    document.getElementById("count").innerHTML=cart.length;
+    if(cart.length==0){
+        document.getElementById('cartItem').innerHTML = "Your cart is empty";
+        document.getElementById("total").innerHTML = "₹ "+0+".00";
+    }
+    else{
+        document.getElementById("cartItem").innerHTML = cart.map((items)=>
+        {
+            var {image, title, price} = items;
+            total=total+price;
+            document.getElementById("total").innerHTML = "₹ "+total+".00";
+            return(
+                `<div class='cart-item'>
+                <div class='row-img'>
+                    <img class='rowimg' src=${image}>
+                </div>
+                <p style='font-size:12px;'>${title}</p>
+                <h2 style='font-size: 15px;'>₹ ${price}.00</h2>`+
+                "<i class='fa-solid fa-trash' onclick='delElement("+ (j++) +")'></i></div>"
+            );
+        }).join('');
+    }
+
+    
+}
